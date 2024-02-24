94ecb61bceb3ecb476e1070701b7482b298c228e
trinhphu9872 <trinhphu9872@gmail.com>
2024-02-24 19:55:30 +0700

    base-mg

--- price-tracker/components/Navbar.tsx
+++ price-tracker/components/Navbar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
+
+const NavIcons = [
+    { src: '/assets/icons/search.svg', alt: 'search' },
+    { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+    { src: '/assets/icons/user.svg', alt: 'user' },
+]
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <header className='w-full'>
+            <nav className='nav'>
+                <Link href="/" className="flex items-center gap-1">
+                    <Image
+                        src="./assets/icons/logo.svg"
+                        width={27}
+                        height={27}
+                        alt="logo"
+                    />
+                    <p className="nav-logo">
+                        Price<span className="text-primary">Wise</span>
+                    </p>
+                </Link>
+                <div className="flex items-center gap-5">
+                    {NavIcons.map((icon) => (
+                        <Image
+                            key={icon.alt}
+                            src={icon.src}
+                            width={28}
+                            height={28}
+                            alt={icon.alt}
+                        />
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
