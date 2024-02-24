da99befea963fbf807ea38344fd2d75845b3b65c
trinhphu9872 <trinhphu9872@gmail.com>
2024-02-24 18:56:02 +0700

    base

---
+++ price-tracker/app/layout.tsx
@@ -0,0 +1,31 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
+})
+
+export const metadata: Metadata = {
+  title: "Price Tracker",
+  description: "Track product online",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+        </main>
+      </body>
+    </html>
+  );
+}
